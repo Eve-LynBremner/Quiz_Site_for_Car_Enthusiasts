e23e2539ded2c49bf699e34e1d2d4704c73aa391
Alan Thomas <alanthomas171@gmail.com>
2026-03-17 19:46:56 +0000

    chore/ add tailwind css and base config

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -19,14 +19,34 @@ const Quiz = () => {
       difficulty: "easy",
       image: null,
     },
+    {
+      id: 3,
+      question: "Which company manufactures the Civic?",
+      options: ["Honda", "Toyota", "Nissan", "Mazda"],
+      answer: "Honda",
+      category: "Brands",
+      difficulty: "easy",
+      image: null,
+    },
+    {
+      id: 4,
+      question: "Which brand is known for the 911 sports car?",
+      options: ["Porsche", "Audi", "BMW", "Mercedes-Benz"],
+      answer: "Porsche",
+      category: "Brands",
+      difficulty: "easy",
+      image: null,
+    },
   ];
   return (
     <div>
       <h2>Quiz</h2>
-      <div className="course-list">
+      <div className="flex flex-col gap-4">
         {questions.map((question) => (
           <QuizQuestion key={question.id} question={question} />
         ))}
+
+        <button className="bg-green-500">Submit</button>
       </div>
     </div>
   );
--- client/src/components/QuizQuestion.jsx
+++ client/src/components/QuizQuestion.jsx
@@ -1,8 +1,8 @@
 const QuizQuestion = ({ question }) => {
   return (
     <div className="card">
-      <div className="card-title">Course Number: {question.id}</div>
-      <div className="card-description">Question: {question.question}</div>
+      <div className="">Course Number: {question.id}</div>
+      <div className="">Question: {question.question}</div>
       <select>
         {question.options.map((fruit, index) => (
           <option key={index} value={fruit}>
