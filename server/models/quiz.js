d7eb3534f7817a830d7f9a9b207d217a07b75ff0
Eve-LynBremner <emvanschoor@gmail.com>
2026-03-19 19:34:06 +0000

    update model files

---
+++ server/models/quiz.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class Quiz extends Model {}
+
+
+Question.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        quizName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        difficulty: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "quiz",
+    }
+);
+
+module.exports = Quiz;
