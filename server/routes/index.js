aa7d1936458ede68cad8fcd32855dcc39d8b7058
Eve-LynBremner <emvanschoor@gmail.com>
2026-03-19 19:43:30 +0000

    update leader and index routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,18 @@
 const router = require("express").Router();
 
-const courseRoutes = require("./course");
-const categoryRoutes = require("./category");
+const questionRoutes = require("./question");
+const leaderRoutes = require("./leader");
 const userRoutes = require("./user");
+const quizRoutes = require("./quiz");
 
 // create a default route for /api
 router.get("/api", (req, res) => {
   res.json({ message: "Welcome to the API" });
 });
 
-router.use("/api/categories", categoryRoutes);
-router.use("/api/courses", courseRoutes);
+router.use("/api/leaders", leaderRoutes);
+router.use("/api/questions", questionRoutes);
 router.use("/api/users", userRoutes);
+router.use("/api/quizzes", quizRoutes);
 
 module.exports = router;
