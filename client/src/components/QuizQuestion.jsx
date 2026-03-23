e5281fd5dead99235640f30d4ac67b68e7716a5e
Alan Thomas <alanthomas171@gmail.com>
2026-03-23 19:25:14 +0000

    Attempt Picture

--- client/src/components/QuizQuestion.jsx
+++ client/src/components/QuizQuestion.jsx
@@ -4,6 +4,17 @@ const QuizQuestion = ({ question, onAnswerChange }) => {
     <div className="card bg-gray-900">
       <div>Course Number: {question.id}</div>
       <div>Question: {question.question}</div>
+
+      {question.image && (
+        <div className="mb-4">
+          <img
+            src={question.image}
+            alt="Quiz context"
+            className="max-w-full h-auto rounded-md border border-gray-600"
+          />
+        </div>
+      )}
+
       <select onChange={(e) => onAnswerChange(question.id, e.target.value)}>
         <option value="">Select an answer</option>
         {question.options.map((fruit, index) => (
