a1dd1fb105f11301553b70ea30312d8dcfe2077f
Alan Thomas <alanthomas171@gmail.com>
2026-03-23 20:38:15 +0000

    tweak/ Move and fix images for questions

--- client/src/components/QuizCard.jsx
+++ client/src/components/QuizCard.jsx
@@ -10,21 +10,26 @@ const QuizCard = ({ quiz }) => {
       hard: "border-red-500",
     }[quiz.difficulty.toLowerCase()] || "border-gray-300";
 
+  const bgColor =
+    {
+      easy: "bg-green-500",
+      medium: "bg-yellow-500",
+      hard: "bg-red-500",
+    }[quiz.difficulty.toLowerCase()] || "border-gray-300";
+
   return (
-    <div className={`card border-2 ${borderColor} p-4 rounded-lg text-white`}>
+    <div className={`card border-4 ${borderColor} p-8 rounded-4xl text-white`}>
       <div className="card text-white">
-        <div className="card-description text-white">
-          Course Title: {quiz.id}
-        </div>
-        <div className="card-description text-white">
-          Course Title: {quiz.quizName}
-        </div>
-
-        <div className="card-description text-white">
-          Course Title: {quiz.difficulty}
+        <div
+          className={`card border-4 ${bgColor} ${borderColor} p-8 rounded-4xl text-white`}
+        >
+          {quiz.quizName}
         </div>
 
-        <div className="card-description">Course Title: {quiz.category}</div>
+        <div className="card-description text-white">Quiz Id{quiz.id}</div>
+        <div className=" text-white">{quiz.difficulty}</div>
+
+        <div className="card-description">Quiz Category: {quiz.category}</div>
         <Link
           to={`/quiz/${quiz.id}`}
           className="mt-4 inline-block px-4 py-2  font-semibold rounded-md bg-white text-black"
